6ac714ff5b1dab60193d538a99e4d84adba8e385
Joscha <joscha@schnipseljagd.org>
2015-04-22 22:02:50 +0200

    implement list annotations

--- src/promdash_annotation_server/handler.clj
+++ src/promdash_annotation_server/handler.clj
@@ -1,13 +1,11 @@
 (ns promdash-annotation-server.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
+  (:require [compojure.core :refer :all] [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [cheshire.core :refer :all]
             [taoensso.faraday :as far]))
 
 (def client-opts
-  {:access-key (System/getenv "AWS_ACCESS_KEY")
-   :secret-key (System/getenv "AWS_SECRET_KEY")
+  {:access-key (System/getenv "AWS_ACCESS_KEY") :secret-key (System/getenv "AWS_SECRET_KEY")
    :endpoint (get (System/getenv) "AWS_DYNAMODB_ENDPOINT" "http://localhost:8000")
    :table-name "promdash-annotations"})
 
@@ -20,30 +18,45 @@
                                   :write (get (System/getenv) "DYNAMODB_WRITE_CAPACITY_UNITS" 1)}
                      :block? true}))
 
-(defn delete-tag [tag timestamp]
-  (far/delete-item client-opts (keyword (:table-name client-opts)) {:tag tag :timestamp (str timestamp)}))
+(defn delete-annotation [tag timestamp]
+  (far/delete-item client-opts (keyword (:table-name client-opts)) {:tag tag :timestamp (num timestamp)}))
 
-(defn put-tag [tag timestamp message]
-  (far/put-item client-opts (keyword (:table-name client-opts)) {:tag tag :timestamp (str timestamp) :message message}))
+(defn put-annotation [tag timestamp message]
+  (far/put-item client-opts (keyword (:table-name client-opts)) {:tag tag :timestamp (num timestamp) :message message}))
 
-(defn list-annotations [& {:keys [tags mrange until]}]
-  [])
+(defn list-annotations [& {:keys [tag mrange until]}]
+  (let [annotations (far/query client-opts
+                               (keyword (:table-name client-opts))
+                               {:tag [:eq tag]
+                                :timestamp [:between [(- until mrange) until]]})]
+    (map (fn [annotation] {:created_at (:timestamp annotation) :message (:message annotation)}) annotations)))
 
 (comment
-  (delete-tag "foo" 123)
-  (put-tag "foo" 123 "hello!")
-  (list-annotations :tags ["foo" "bar"] :range 3600 :until 123))
+  (delete-annotation "foo" 123)
+  (ensure-annotations-table-exists)
+  (put-annotation "foo" 123 "hello!")
+  (put-annotation "bar" 123 "hello!")
+  (put-annotation "foo" 1 "hello!")
+  (put-annotation "foo" 23 "hello!")
+  (put-annotation "foo" 2345 "huhu")
+  (list-annotations :tag "foo" :mrange 100 :until 123))
+
+(defn annotation-list-response [tags mrange until]
+  {:posts (into [] (flatten (map #(list-annotations :tag % :mrange mrange :until until) tags)))})
+
+(defn current-timestamp []
+  (int (/ (System/currentTimeMillis) 1000)))
 
 (defroutes app-routes
-           (GET "/annotations" [request]
-                (let [tags (get-in request [:query-params :tags] [])
-                      mrange (get-in request [:query-params :range] 60)
-                      until (get-in request [:query-params :until] (/ (System/currentTimeMillis) 1000))]
-                     (generate-string {:posts (list-annotations :tags tags :range mrange :until until)})))
-           (PUT "/annotations/tags/:tag" request
+           (GET "/annotations" request
+                (let [tags (get-in request [:params "tags[]"] [])
+                      mrange (read-string (get-in request [:params :range] "60"))
+                      until (read-string (get-in request [:params :until] (str (current-timestamp))))]
+                     (generate-string (annotation-list-response tags mrange until))))
+           (POST "/annotations/tags/:tag" request
                 (let [tag (get-in request [:params :tag])
                       body (parse-string (slurp (get request :body)) true)]
-                  (put-tag tag (:created_at body) (:message body)))
+                  (put-annotation tag (:created_at body) (:message body)))
                  "ok")
            (route/not-found "Not Found"))
 
--- test/promdash_annotation_server/handler_test.clj
+++ test/promdash_annotation_server/handler_test.clj
@@ -5,11 +5,10 @@
             [cheshire.core :refer :all]))
 
 (def some-deployment-annotation-post
-  {:created_at 111232553 :message "some deployment"})
+  {:created_at 1423487712 :message "some deployment"})
 
 (use-fixtures :once (fn [f] (ensure-annotations-table-exists) (f)))
-
-(use-fixtures :each (fn [f] (delete-tag "deployment" 111232553) (f)))
+(use-fixtures :each (fn [f] (delete-annotation "deployment" 1423487712) (f)))
 
 (deftest test-app
   (testing "returns a empty list of annotation posts"
@@ -18,7 +17,7 @@
       (is (= (:body response) (generate-string {:posts []})))))
 
   (testing "returns a list of annotation posts"
-    (let [response (app (mock/body (mock/request :put "/annotations/tags/deployment") (generate-string some-deployment-annotation-post)))]
+    (let [response (app (mock/body (mock/request :post "/annotations/tags/deployment") (generate-string some-deployment-annotation-post)))]
       (is (= (:status response) 200))
       (is (= (:body response) "ok")))
     (let [response (app (mock/request :get "/annotations?range=3600&tags[]=deployment&tags[]=prometheus&until=1423491311.424"))]
