65108b80619a7d019fc7c11e41ca5c1cbf2299ce
Joscha <joscha@schnipseljagd.org>
2015-09-23 23:30:21 +0200

    update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject promdash-annotation-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An annotation backend for promdash."
+  :url "https://github.com/schnipseljagd/promdash-annotation-server"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
